821ea8ab43a5a7652694c53a2478f8959932668b
Imaad Schuller <imaadschullerr@gmail.com>
2024-10-04 21:02:37 +0200

    Add home page route

--- task_management_api/task_management_api/urls.py
+++ task_management_api/task_management_api/urls.py
@@ -4,10 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("task_management_api.tasks.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', views.home),
 ]
