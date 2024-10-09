dc15227b3dc6f8f0f40b58884f138cfccb1a89b8
Imaad Schuller <imaadschullerr@gmail.com>
2024-10-09 09:19:53 +0200

    Create profile admin model

--- task_management_api/tasks/admin.py
+++ task_management_api/tasks/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task, Profile, TaskCategory
 
 admin.site.register(Task)
+admin.site.register(Profile)
+admin.site.register(TaskCategory)
--- task_management_api/tasks/models.py
+++ task_management_api/tasks/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Profile model to extend the User model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
 # Task category model
 class TaskCategory(models.Model):
     name = models.CharField(max_length=100)
