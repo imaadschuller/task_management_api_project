83b515b01ca3938bad2ecef22401c97208a2ee45
Imaad Schuller <imaadschullerr@gmail.com>
2024-10-09 18:52:11 +0200

    Delete previous edit to url routing

--- task_management_api/tasks/urls.py
+++ task_management_api/tasks/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path("", include(router.urls)),
     path("tasks/<int:pk>/complete/", TaskViewSet.as_view({"post": "mark_complete"})),
     path("tasks/<int:pk>/incomplete/", TaskViewSet.as_view({"post": "mark_incomplete"})),
-    path("api/", include("tasks.urls")),
     path('api/register/', views.custom_register, name='custom_register'),
     path('api/login/', views.custom_login, name='custom_login'),
 ]
