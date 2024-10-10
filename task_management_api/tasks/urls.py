0662179a327e8d684fc0d7e106e5238b1dba7ecf
Imaad Schuller <imaadschullerr@gmail.com>
2024-10-10 11:12:39 +0200

    Edit url routing for register and login

--- task_management_api/tasks/urls.py
+++ task_management_api/tasks/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path("", include(router.urls)),
     path("tasks/<int:pk>/complete/", TaskViewSet.as_view({"post": "mark_complete"})),
     path("tasks/<int:pk>/incomplete/", TaskViewSet.as_view({"post": "mark_incomplete"})),
-    path('api/register/', views.custom_register, name='custom_register'),
-    path('api/login/', views.custom_login, name='custom_login'),
+    path('register/', views.custom_register, name='custom_register'),
+    path('login/', views.custom_login, name='custom_login'),
 ]
